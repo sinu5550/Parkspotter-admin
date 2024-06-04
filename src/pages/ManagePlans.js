9f45909c9fcd4d130482003f456f452b294c5e62
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-06-04 10:51:33 +0600

    [fixed]

--- src/pages/ManagePlans.js
+++ src/pages/ManagePlans.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+// import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import SubscriptionModal from "./SubscriptionModal"
 import { IoIosAddCircleOutline } from "react-icons/io"
